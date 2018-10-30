a93c5389cd96d9c8257f155422f1373c39af8e1a
Mike Caruso <5948782+MichaelJCaruso@users.noreply.github.com>
2018-10-30 14:51:26 -0400

    Implement environment variable based configuration
    Next up, the README :-)

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
-const express = require('express');
-const fs = require('fs');
-const helmet = require('helmet');
-const http = require('http');
-const https = require('https');
-const morgan = require('morgan');
-const path = require('path');
+/*----------------------------*
+ *----  Required Modules  ----*
+ *----------------------------*/
+
+const express = require('express');       //  ... npm
+const helmet  = require('helmet');        //  ... npm
+const morgan  = require('morgan');        //  ... npm
+
+const fs    = require('fs');              //  ... node built-in
+const http  = require('http');            //  ... node built-in
+const https = require('https');           //  ... node built-in
+const path  = require('path');            //  ... node built-in
+
+/*----------------------------------------*
+ *----                                ----*
+ *----  Configuration Access Utility  ----*
+ *----                                ----*
+ *----------------------------------------*/
+
+class Configuration {
+    constructor (configurationName) {
+        this.environmentPrefix = (
+            process.env[configurationName]
+                ? process.env[configurationName]
+                : `${configurationName}`
+        ).concat ('_');
+    }
+
+    getVariable (item) {
+        return this.environmentPrefix.concat (item);
+    }
+    getItem (item) {
+        return process.env[this.getVariable (item)];
+    }
+
+    deleteItem (item) {
+	delete process.env[this.getVariable (item)];
+    }
+
+    ifItem (...items) { // === ifItem (item, item ..., callback)
+	// Remove the 'callback' from the end of the item list...
+        const callback=items.pop ();
+	/// ... and call on the item values ...
+        return this.onItem (
+            ...items,
+	    (...items)=>(
+		// ... iff no item value is 'false' or 'undefined':
+                items.reduce ((cum,cur)=>cum && cur,true) && callback(...items)
+	    )
+        );
+    }
+    onItem (...items) { // === onItem (item, item ..., callback)
+	// Remove the 'callback' from the end of the item list...
+	const callback=items.pop ();
+	// ... and apply it to the item values:
+	return callback (...items.map (item=>this.getItem (item)));
+    }
+}
+
+/*--------------------------*
+ *----                  ----*
+ *----  The Main Event  ----*
+ *----                  ----*
+ *--------------------------*/
+
+/*-------------------------*
+ *----  configuration  ----*
+ *-------------------------*/
+
+const MyConfiguration = new Configuration ('vision_xa_nodejs_serve');
 
 /*-------------------*/
 /*----  express  ----*/
@@ -12,66 +75,71 @@ const path = require('path');
 const app = express ();
 module.exports.app = app;
 
-/*--------------------------*
- *----  best practices  ----*
- *--------------------------*/
+/*--------------------------------------*
+ *----  basic boilerplate security  ----*
+ *--------------------------------------*/
 app.use (helmet ());
 
 /*-------------------*/
 /*----  logging  ----*/
 /*-------------------*/
-app.use (
-    morgan (
-        'combined', {
-            stream: fs.createWriteStream(
-                path.join(__dirname, '/logs', 'access.log'), {flags: 'a'}
-            )
-        }
+MyConfiguration.ifItem (
+    'log_file', logfile=>app.use (
+        morgan (
+            'combined', {
+                stream: fs.createWriteStream(logfile, {flags: 'a'})
+            }
+        )
     )
 );
 
-/*--------------------------*/
-/*----  static content  ----*/
-/*--------------------------*/
-app.use (express.static (path.join (__dirname, 'public')));
+/*---------------------------------*/
+/*----  '/vision/api' handler  ----*/
+/*---------------------------------*/
 
-/*-------------------*/
-/*----  servers  ----*/
-/*-------------------*/
-const httpServer = http.createServer (app);
-
-/*-----------------------*/
-/*----  http server  ----*/
-/*-----------------------*/
-const httpsServer = https.createServer ({
-    key : fs.readFileSync('./config/certs/key.pem'),
-    cert: fs.readFileSync('./config/certs/cert.pem')
-}, app);
-
-/*--------------------------*/
-/*----  the good stuff  ----*/
-/*--------------------------*/
-
-/*----------------*/
 const visionServer = require('./lib/vision_server');
 module.exports.visionServer = visionServer;
 app.use ('/vision/api', visionServer.app);
 
-/*----------------*/
-const ports = (port=>({
-    http : +port,
-    https: +port+443
-}))(process.env.PORT || 3001);
-module.exports.ports = ports;
+/*--------------------------*
+ *----  web servers...  ----*
+ *--------------------------*/
 
 const servers = {
-    http : httpServer,
-    https: httpsServer
 };
-module.exports.servers = servers;
 
-['http','https'].map(
-    k=>((k,s,p)=>s.listen(p,()=>{
-        console.log(`'${k}' server listening on port ${p}`);
-    }))(k,servers[k],ports[k])
+/*--------------------*
+ *----  ... http  ----*
+ *--------------------*/
+
+MyConfiguration.ifItem (
+    'http_port', port=>{
+	const newServer=http.createServer (app);
+        console.log (`Starting http server on port ${port}`);
+        newServer.listen (port);
+        servers.http = newServer;
+    }
+);
+
+/*------------------------*
+ *----  https server  ----*
+ *------------------------*/
+
+MyConfiguration.ifItem (
+    'https_port', 'https_key_file', 'https_cert_file', (port,keyfile,certfile)=>{
+	const httpsOptions = {
+            key  : fs.readFileSync (keyfile),
+            cert : fs.readFileSync (certfile)
+	};
+	MyConfiguration.ifItem (
+	    'https_passphrase', passphrase=>{
+		httpsOptions.passphrase = passphrase;
+		MyConfiguration.deleteItem ('https_passphrase');
+	    }
+	);
+	const newServer=https.createServer (httpsOptions, app);
+        console.log (`Starting https server on port ${port}`);
+        newServer.listen (port);
+	servers.https=newServer;
+    }
 );
